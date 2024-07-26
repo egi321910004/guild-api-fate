18bb0de136c9041a69d1e3604c0d197212e234ba
egi.ramadhani <321910004@student.machung.ac.id>
2024-07-27 01:50:57 +0700

    Add: Cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Fate API')
